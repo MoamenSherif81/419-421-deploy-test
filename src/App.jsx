f18819c8529cc3feefc3d2e27460c29daa02da24
Moamen Sherif <moamenyt@gmail.com>
2023-12-15 22:24:28 +0200

    Add some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import About from './pages/About'
+import { Link } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/shop'>Shop</Link>
+        <Link to='/about'>About</Link>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/shop' element={<Shop />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,6 @@
+
+export default function shop() {
+  return (
+    <div>shop</div>
+  )
+}
